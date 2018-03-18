edce12a50264bd99a1812a8a3e14dd5ecd443a7b
nguy4227 <36711210+nguy4227@users.noreply.github.com>
2018-03-18 16:44:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".btn-github").hover(function () {
+    $(".fa-github").toggleClass("resize");
+});
+
+$(".btn-linkedin").hover(function () {
+    $(".fa-linkedin").toggleClass("resize");
+});
+
+$(".btn-facebook").hover(function () {
+    $(".fa-facebook").toggleClass("resize");
+});
